c7b4e12268d683108f67d2883bba868fbecbecda
muhamadrzl <rizalridlo97@gmail.com>
2023-09-14 15:07:02 +0700

    @Controller, @Repository, @Service

---
+++ src/main/java/net/javaguides/springannotations/controller/MyController.java
@@ -0,0 +1,10 @@
+package net.javaguides.springannotations.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public String hello(){
+        return "Hello Controller";
+    }
+}
---
+++ src/main/java/net/javaguides/springannotations/repository/MyRepository.java
@@ -0,0 +1,10 @@
+package net.javaguides.springannotations.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MyRepository {
+    public String hello(){
+        return "Hellow Repository";
+    }
+}
