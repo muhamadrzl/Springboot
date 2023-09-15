cf99fabf523acf6d42c63b43bc84d5a4a01bbaec
muhamadrzl <rizalridlo97@gmail.com>
2023-09-15 15:59:43 +0700

    RestController

--- src/main/java/net/javaguides/springannotations/controller/BookController.java
+++ src/main/java/net/javaguides/springannotations/controller/BookController.java
@@ -2,18 +2,19 @@ package net.javaguides.springannotations.controller;
 
 import net.javaguides.springannotations.beans.Book;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/api")
 public class BookController {
     @RequestMapping("/hello-world")
-    @ResponseBody
     public String helloWorld(){
         return "Hello World";
     }
-    @RequestMapping("/book")
-    @ResponseBody
+    @GetMapping(value = {"/book", "/core-java", "/java"})
     public Book getBook(){
         Book book = new Book(1, "Core Java", "Java is Fun");
         return book;
