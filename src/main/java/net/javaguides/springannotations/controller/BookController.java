e7e5680fc2efe845329ab89bb5b23ee4a6847b25
Muhamad Rizal Ridlo Prakoso <rizalridlo97@gmail.com>
2023-09-16 11:13:05 +0700

    Put Method

--- src/main/java/net/javaguides/springannotations/controller/BookController.java
+++ src/main/java/net/javaguides/springannotations/controller/BookController.java
@@ -1,11 +1,11 @@
 package net.javaguides.springannotations.controller;
 
 import net.javaguides.springannotations.beans.Book;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -18,5 +18,23 @@ public class BookController {
     public Book getBook(){
         Book book = new Book(1, "Core Java", "Java is Fun");
         return book;
+    }
+
+    @PostMapping(value = "/books/create",
+    consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public ResponseEntity<Book> createBook(@RequestBody Book book){
+        System.out.println(book.getId());
+        System.out.println(book.getTitle());
+        System.out.println(book.getDescription());
+        return new ResponseEntity<>(book, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value="/books/update/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable int id, @RequestBody Book updatedBook){
+        System.out.println(id);
+        System.out.println(updatedBook.getTitle());
+        System.out.println(updatedBook.getDescription());
+        return ResponseEntity.ok(updatedBook);
     }
 }
